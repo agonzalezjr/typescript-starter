48efa1bbbed87751c2e9138b210751cd993317d0
Andy Gonzalez <andy.gonzalez@sap.com>
2019-06-14 15:33:38 -0400

    Add timer/tracing capabilities

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
-module.exports =  {
-  "semi": true,
-  "trailingComma": "all",
-  "printWidth": 120,
-  "tabWidth": 2,
-  "singleQuote": true
-}
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  printWidth: 120,
+  tabWidth: 2,
+  singleQuote: true,
+};
